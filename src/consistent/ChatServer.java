cc0a780230aa517dc1ec7043a31a15eaa12d641e
Derek McLean <derek.l.mclean@gmail.com>
2012-03-31 01:28:48 -0700

    Chat server that runs

--- src/consistent/ChatServer.java
+++ src/consistent/ChatServer.java
@@ -2,18 +2,44 @@ package consistent;
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class ChatServer {
-	public static void main (String args[]) {
-		try{
-			int serverPort = 7896;
+	private static final int SERVER_SOCKET_TIMEOUT = 500;
+
+	public static void main(String args[]) {
+		try {
+			int serverPort = Integer.parseInt(args[0]);
 			ServerSocket listenSocket = new ServerSocket(serverPort);
-			while(true) {
-				Socket clientSocket = listenSocket.accept();
-				Connection c = new Connection(clientSocket);
+			listenSocket.setSoTimeout(SERVER_SOCKET_TIMEOUT);
+
+//			ConnectionManager cm = new ConnectionManager(listenSocket);
+			List<Connection> clients = new LinkedList<Connection>(); //cm.getConnectionPool();
+//			cm.start();
+
+			while (true) {
+				try {
+					Socket clientSocket = listenSocket.accept();
+					Connection c = new Connection(clientSocket);
+					clients.add(c);
+					System.out.println("Added connection");
+				} catch (SocketTimeoutException e) { }
+				//synchronized (clients) {
+					for (Connection source : clients) {
+						String message = source.readMessage();
+						if (message != null) {
+						for (Connection dest : clients) {
+							if (source != dest)
+								dest.writeMessage(message);
+						}
+						}
+					}
+				//}
 			}
-		} catch(IOException e) {
-			System.out.println("Listen :"+e.getMessage());
+		} catch (IOException e) {
+			System.out.println("Listen :" + e.getMessage());
+		} catch (NumberFormatException e) {
+			System.out.println("Bad Server Port!");
 		}
 	}
 }
--- src/consistent/Connection.java
+++ src/consistent/Connection.java
@@ -9,32 +9,59 @@ class Connection extends Thread {
 	DataOutputStream out;
 	Socket clientSocket;
 	
+	private String name;
+	
 	public Connection (Socket aClientSocket) {
 		try {
 			clientSocket = aClientSocket;
 			in = new DataInputStream( clientSocket.getInputStream());
-			out =new DataOutputStream( clientSocket.getOutputStream());
-			this.start();
+			out = new DataOutputStream( clientSocket.getOutputStream());
+			
+			name = in.readUTF();
+			System.out.println("Connected: " + name);
+			
+//			this.start();
 		} catch(IOException e) {
 			System.out.println("Connection:"+e.getMessage());
 		}
 	}
 	
 	public void run() {
+//		try {
+//			// an echo server
+//			String data = in.readUTF();
+//			out.writeUTF(data);
+//		} catch(EOFException e) {
+//			System.out.println("EOF:"+e.getMessage());
+//		} catch(IOException e) {
+//			System.out.println("IO:"+e.getMessage());
+//		} finally {
+//			try {
+//				clientSocket.close();
+//			} catch (IOException e) {
+//				/*close failed*/
+//			}
+//		}
+	}
+
+	public String readMessage() {
 		try {
-			// an echo server
-			String data = in.readUTF();
-			out.writeUTF(data);
-		} catch(EOFException e) {
-			System.out.println("EOF:"+e.getMessage());
-		} catch(IOException e) {
-			System.out.println("IO:"+e.getMessage());
-		} finally {
-			try {
-				clientSocket.close();
-			} catch (IOException e) {
-				/*close failed*/
+			if (in.available() > 0) {
+				String msg = in.readUTF();
+				System.out.println("Saw message from " + name + ": " + msg);
+				return String.format("%s: %s", name, msg);
 			}
+		} catch (IOException e) { }
+		return null;
+	}
+
+	public boolean writeMessage(String message) {
+		try {
+			System.out.println("Delivering Message To: " + name + ":: " + message);
+			out.writeUTF(message);
+		} catch (IOException e) {
+			return false;
 		}
+		return true;
 	}
 }

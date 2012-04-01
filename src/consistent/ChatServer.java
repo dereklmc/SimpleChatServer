9949bfc3a40ac74bfec7c131c4aca87e7214baad
Derek McLean <derek.l.mclean@gmail.com>
2012-04-01 00:16:16 -0700

    Object Oriented Messaging

--- src/consistent/ChatServer.java
+++ src/consistent/ChatServer.java
@@ -7,37 +7,39 @@ import java.util.*;
 public class ChatServer {
 
 	private static final int SERVER_SOCKET_TIMEOUT = 500;
-	
+
 	private ServerSocket listenSocket;
 	private List<Connection> clients;
-	
+
 	public ChatServer(int serverPort) throws IOException {
 		listenSocket = new ServerSocket(serverPort);
 		listenSocket.setSoTimeout(SERVER_SOCKET_TIMEOUT);
-		
+
 		clients = new LinkedList<Connection>();
 	}
-	
+
 	public void acceptNewConnections() throws IOException {
 		try {
-		Socket clientSocket = listenSocket.accept();
-		Connection c = new Connection(clientSocket);
-		clients.add(c);
-		} catch (SocketTimeoutException e) { }
+			Socket clientSocket = listenSocket.accept();
+			Connection c = new Connection(clientSocket);
+			clients.add(c);
+		} catch (SocketTimeoutException e) {
+		}
 	}
-	
+
 	public void processMessages() {
 		for (Connection source : clients) {
-			String message = source.readMessage();
-			if (message != null) {
-				for (Connection dest : clients) {
-					if (source != dest)
-						dest.writeMessage(message);
-				}
+			Connection.Message message = source.readMessage();
+			if (message == null) {
+				continue;
+			}
+			for (Connection dest : clients) {
+				if (source != dest)
+					dest.writeMessage(message);
 			}
 		}
 	}
-	
+
 	public static void main(String args[]) {
 		try {
 			int serverPort = Integer.parseInt(args[0]);
--- src/consistent/Connection.java
+++ src/consistent/Connection.java
@@ -3,62 +3,57 @@ package consistent;
 import java.io.*;
 import java.net.*;
 
-class Connection extends Thread {
-	
+class Connection {
+
+	public class Message {
+
+		private String senderName;
+		private String messageBody;
+
+		public Message(String senderName, String messageBody) {
+			this.senderName = senderName;
+			this.messageBody = messageBody;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("%s: %s", senderName, messageBody);
+		}
+	}
+
 	DataInputStream in;
 	DataOutputStream out;
 	Socket clientSocket;
-	
+
 	private String name;
-	
-	public Connection (Socket aClientSocket) {
+
+	public Connection(Socket aClientSocket) {
 		try {
 			clientSocket = aClientSocket;
-			in = new DataInputStream( clientSocket.getInputStream());
-			out = new DataOutputStream( clientSocket.getOutputStream());
-			
+			in = new DataInputStream(clientSocket.getInputStream());
+			out = new DataOutputStream(clientSocket.getOutputStream());
+
 			name = in.readUTF();
 			System.out.println("Connected: " + name);
-			
-//			this.start();
-		} catch(IOException e) {
-			System.out.println("Connection:"+e.getMessage());
+
+		} catch (IOException e) {
+			System.out.println("Connection:" + e.getMessage());
 		}
 	}
-	
-	public void run() {
-//		try {
-//			// an echo server
-//			String data = in.readUTF();
-//			out.writeUTF(data);
-//		} catch(EOFException e) {
-//			System.out.println("EOF:"+e.getMessage());
-//		} catch(IOException e) {
-//			System.out.println("IO:"+e.getMessage());
-//		} finally {
-//			try {
-//				clientSocket.close();
-//			} catch (IOException e) {
-//				/*close failed*/
-//			}
-//		}
-	}
 
-	public String readMessage() {
+	public Message readMessage() {
 		try {
 			if (in.available() > 0) {
-				String msg = in.readUTF();
-				System.out.println("Saw message from " + name + ": " + msg);
-				return String.format("%s: %s", name, msg);
+				return new Message(name, in.readUTF());
 			}
-		} catch (IOException e) { }
+		} catch (IOException e) {
+		}
 		return null;
 	}
 
-	public boolean writeMessage(String message) {
+	public boolean writeMessage(Message message) {
 		try {
-			System.out.println("Delivering Message To: " + name + ":: " + message);
-			out.writeUTF(message);
+			out.writeUTF(message.toString());
 		} catch (IOException e) {
 			return false;
 		}

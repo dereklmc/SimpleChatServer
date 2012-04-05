4a0560abf5019695bfad17fdb57cdd5ad46531dd
Derek McLean <derek.l.mclean@gmail.com>
2012-04-05 14:40:43 -0700

    Started Casual Ordered Chat program

---
+++ src/casual/Chat.java
@@ -0,0 +1,15 @@
+package casual;
+
+public class Chat {
+	
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			System.err.println("Syntax: java Chat <port> "
+					+ "<node 1 ip name> ... <node n ip name>");
+			System.exit(1);
+		}
+		
+		int port = Integer.parseInt(args[0]);
+		Arrays.
+	}
+}
